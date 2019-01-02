23636c0c9282161e06132c9e5742b9df82e80862
reco_luan <luanhewei@gmail.com>
2019-01-02 13:50:56 +0800

    test

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,7 +1,14 @@
 import addRoutes from './theme/router'
 export default ({
-  router
+  Vue,
+  router,
+  options,
+  siteData
 }) => {
   // ...做一些其他的应用级别的优化
   addRoutes(router)
+  router.beforeEach((to, from, next) => {
+    console.log(from, to)
+    next()
+  })
 }
