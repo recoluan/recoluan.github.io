366787743b2b6f6292be072f9217bf9938f96aa0
reco_luan <luanhewei@gmail.com>
2019-05-22 16:58:10 +0800

    优化配置

---
+++ docs/.vuepress/config/nav/index.js
@@ -0,0 +1,16 @@
+module.exports = [
+  { text: 'Home', link: '/', icon: 'reco-home' },
+  { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
+  { text: 'Three-Month Study', link: '/views/threeMonthStudy/', icon: 'reco-three' },
+  { text: 'Contact', 
+    icon: 'reco-message',
+    items: [
+      { text: 'NPM', link: 'https://www.npmjs.com/~reco_luan', icon: 'reco-npm' },
+      { text: 'GitHub', link: 'https://github.com/recoluan', icon: 'reco-github' },
+      { text: '简书', link: 'https://www.jianshu.com/u/cd674a19515e', icon: 'reco-jianshu' },
+      { text: 'CSDN', link: 'https://blog.csdn.net/recoluan', icon: 'reco-csdn' },
+      { text: '博客圆', link: 'https://www.cnblogs.com/luanhewei/', icon: 'reco-bokeyuan' },
+      { text: 'WeChat', link: 'https://mp.weixin.qq.com/s/mXFqeUTegdvPliXknAAG_A', icon: 'reco-wechat' },
+    ]
+  }
+]
---
+++ docs/.vuepress/config/theme/themeReco.js
@@ -0,0 +1,35 @@
+module.exports = {
+  // 博客设置
+  blogConfig: {
+    category: {
+      location: 2, // 在导航栏菜单中所占的位置，默认2
+      text: 'Category' // 默认 “分类”
+    },
+    tag: {
+      location: 3, // 在导航栏菜单中所占的位置，默认3
+      text: 'Tag' // 默认 “标签”
+    }
+  },
+  // 最后更新时间
+  lastUpdated: 'Last Updated', // string | boolean
+  // 作者
+  author: 'reco_luan',
+  // 密钥
+  // keyPage: {
+  //   keys: ['930105'],
+  //   color: '#42b983',
+  //   lineColor: '#42b983'
+  // },
+  // valine 设置
+  valineConfig: {
+    appId: 'Q6hMeY2PSaM9FMkXetzoJoU5-gzGzoHsz',
+    appKey: 'iLQlev5jo2Cm5pLcI0z3qhtr',
+    placeholder: '填写邮箱可以收到回复提醒哦！',
+    notify: true,
+    recordIP: true
+  },
+  // 备案号
+  record: '京ICP备17067634号-1',
+  // 项目开始时间
+  startYear: '2017'
+}
