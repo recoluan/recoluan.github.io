c4201e0291d9341bde3c6bd82f7f1ed43891d400
reco_luan <luanhewei@gmail.com>
2019-06-24 10:22:39 +0800

    清空主题文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,7 @@ module.exports = {
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
   ],
-  // theme: 'reco',
+  theme: 'reco',
   themeConfig,
   markdown: {
     lineNumbers: true
--- docs/.vuepress/theme/index.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('../tm/index')
