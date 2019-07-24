1b2c59f35cd46132e0a9f7bc3ba6dfac789631b6
reco_luan <recoluan@qq.com>
2019-07-24 15:15:38 +0800

    add: article

--- docs/.vuepress/config/nav/index.js
+++ docs/.vuepress/config/nav/index.js
@@ -1,7 +1,6 @@
 module.exports = [
   { text: 'Home', link: '/', icon: 'reco-home' },
   { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
-  // { text: 'Three-Month Study', link: '/views/threeMonthStudy/', icon: 'reco-three' },
   { text: 'Contact', 
     icon: 'reco-message',
     items: [
