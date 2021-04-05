54a0c0b0c9acb9f20f8726b9babd142ffc002947
reco_luan <recoluan@outlook.com>
2021-04-05 20:46:48 +0800

    feat: add doc

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -8,6 +8,7 @@ module.exports = {
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'baidu_union_verify', content: '0d5c264115348f117421989d01867f8d' }]
     ['meta', { name: 'theme-color', content: '#42b983' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
