816566e1ee3dabf3635b3fccc5927e2acf236a5e
reco_luan <recoluan@qq.com>
2022-07-13 00:15:13 +0800

    feat: add doc

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -18,27 +18,25 @@ export default defineUserConfig({
     docsRepo: 'https://github.com/recoluan/recoluan.github.io',
     docsBranch: 'gh-pages-source',
     lastUpdatedText: '',
-    navbar:
-    [
-      { 
+    navbar: [
+      {
         text: 'Contact',
         children: [
           { text: 'GitHub', link: 'https://github.com/recoluan' },
-          { text: '简书', link: 'https://www.jianshu.com/u/cd674a19515e' },
-          { text: 'CSDN', link: 'https://blog.csdn.net/recoluan' },
-          { text: '博客圆', link: 'https://www.cnblogs.com/luanhewei/' }
         ]
       }
     ],
-    // valine 设置
-    valineConfig: {
-      appId: 'Q6hMeY2PSaM9FMkXetzoJoU5-gzGzoHsz',
-      appKey: 'iLQlev5jo2Cm5pLcI0z3qhtr',
-      placeholder: '填写邮箱可以收到回复提醒哦！',
-      verify: true, // 验证码服务
-      // notify: true, //
-      recordIP: true,
-      // hideComments: true
+    commentConfig: {
+      type: 'valine',
+      options: {
+        appId: 'Q6hMeY2PSaM9FMkXetzoJoU5-gzGzoHsz',
+        appKey: 'iLQlev5jo2Cm5pLcI0z3qhtr',
+        placeholder: '填写邮箱可以收到回复提醒哦！',
+        verify: true, // 验证码服务
+        // notify: true, //
+        recordIP: true,
+        // hideComments: true
+      },
     },
     autoAddCategoryToNavbar: true
   }),
